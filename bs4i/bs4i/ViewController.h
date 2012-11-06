920d5dea48d8d7375c64c1db30ba89f15c241c8d
croath <croathliu@gmail.com>
2012-11-06 23:41:23 +0800

    project created

---
+++ bs4i/bs4i/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  bs4i
+//
+//  Created by  on 12-11-6.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
