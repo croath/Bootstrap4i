6bc5e861df132bfb3f95b05dcdd47530ec708bd6
croath <croathliu@gmail.com>
2012-11-11 23:18:32 +0800

    touchinside colors

--- bs4i/bs4i/BSButton.m
+++ bs4i/bs4i/BSButton.m
@@ -211,9 +211,97 @@
 
 - (void)initTouchInsideLayer{
     touchInsideLayer = [[CAGradientLayer alloc] init];
+    
+    float redUp, greenUp, blueUp, alphaUp, redDown, greenDown, blueDown, alphaDown;
+    alphaUp = alphaDown = 1.0;
+    switch (bsType) {
+        case BSButtonTypePrimary:
+        {
+            redUp = 13.0 / 255.0;
+            greenUp = 71.0 / 255.0;
+            blueUp = 169.0 / 255.0;
+            
+            redDown = 16.0 / 255.0;
+            greenDown = 82.0 / 255.0;
+            blueDown = 195.0 / 255.0;
+        }
+            break;
+        case BSButtonTypeInfo:
+        {
+            redUp = 68.0 / 255.0;
+            greenUp = 129.0 / 255.0;
+            blueUp = 152.0 / 255.0;
+            
+            redDown = 78.0 / 255.0;
+            greenDown = 148.0 / 255.0;
+            blueDown = 175.0 / 255.0;
+        }
+            break;
+        case BSButtonTypeSuccess:
+        {
+            redUp = 89.0 / 255.0;
+            greenUp = 136.0 / 255.0;
+            blueUp = 76.0 / 255.0;
+            
+            redDown = 103.0 / 255.0;
+            greenDown = 156.0 / 255.0;
+            blueDown = 87.0 / 255.0;
+        }
+            break;
+        case BSButtonTypeWarning:
+        {
+            redUp = 203.0 / 255.0;
+            greenUp = 131.0 / 255.0;
+            blueUp = 55.0 / 255.0;
+            
+            redDown = 233.0 / 255.0;
+            greenDown = 150.0 / 255.0;
+            blueDown = 64.0 / 255.0;
+        }
+            break;
+        case BSButtonTypeDanger:
+        {
+            redUp = 150.0 / 255.0;
+            greenUp = 59.0 / 255.0;
+            blueUp = 52.0 / 255.0;
+            
+            redDown = 173.0 / 255.0;
+            greenDown = 68.0 / 255.0;
+            blueDown = 60.0 / 255.0;
+        }
+            break;
+        case BSButtonTypeInverse:
+        {
+            redUp = 29.0 / 255.0;
+            greenUp = 29.0 / 255.0;
+            blueUp = 29.0 / 255.0;
+            
+            redDown = 33.0 / 255.0;
+            greenDown = 33.0 / 255.0;
+            blueDown = 33.0 / 255.0;
+        }
+            break;
+        case BSButtonTypeLink:
+        {
+            redUp = greenUp = blueUp = alphaUp = redDown = greenDown = blueDown = alphaDown = 0.0;
+        }
+            break;
+        default:
+        {
+            redUp = 199.0 / 255.0;
+            greenUp = 199.0 / 255.0;
+            blueUp = 199.0 / 255.0;
+            
+            redDown = 229.0 / 255.0;
+            greenDown = 229.0 / 255.0;
+            blueDown = 229.0 / 255.0;
+        }
+            break;
+    }
+    
     touchInsideLayer.colors = [NSArray arrayWithObjects:
-                               (id)[UIColor colorWithRed:13.0/255.0 green:71.0/255.0 blue:169.0/255.0 alpha:1.0].CGColor, 
-                               (id)[UIColor colorWithRed:17.0/255.0 green:83.0/255.0 blue:196.0/255.0 alpha:1.0].CGColor,
+                               (id)[UIColor colorWithRed:redUp green:greenUp blue:blueUp alpha:alphaUp].CGColor, 
+                               (id)[UIColor colorWithRed:redDown green:greenDown blue:blueDown alpha:alphaDown].CGColor,
                                nil];
 }
 
