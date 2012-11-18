9d8a84ebb784f376fa7509abbb687306e83bf6db
croath <croathliu@gmail.com>
2012-11-18 19:56:32 +0800

    BSTextField

---
+++ bs4i/bs4i/BSTextFIeld.h
@@ -0,0 +1,13 @@
+//
+//  BSTextFIeld.h
+//  bs4i
+//
+//  Created by  on 12-11-18.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSTextFIeld : UITextField
+
+@end
